87ad8baba0107b10590c27fd1188e7b7f421b90d
Haziq <haziqiskandar443@gmail.com>
2024-10-29 22:57:38 +0800

    Improvement on routing

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,9 @@
 <?php
 
-use App\Http\Controllers\WelcomeController;
 use App\Http\Controllers\NoteController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [WelcomeController::class,'welcome'])->name('welcome');
 
 Route::redirect('/', '/note')->name('dashboard');
 
