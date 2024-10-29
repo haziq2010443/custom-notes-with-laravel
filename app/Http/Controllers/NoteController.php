200483d89318a1fca8626c6be25d0970b9e58666
Haziq <haziqiskandar443@gmail.com>
2024-10-29 08:38:58 +0800

    Finished note view and added proper CRUD operation and routing

--- app/Http/Controllers/NoteController.php
+++ app/Http/Controllers/NoteController.php
@@ -29,7 +29,14 @@ class NoteController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $data = $request->validate([
+            'note' => ['required','string']
+        ]);
+
+        $data['user_id'] = 1;
+        $note = Note::create($data);
+
+        return to_route('note.show', $note)->with('message', 'Note was created');
     }
 
     /**
@@ -53,7 +60,13 @@ class NoteController extends Controller
      */
     public function update(Request $request, Note $note)
     {
-        //
+        $data = $request->validate([
+            'note' => ['required','string']
+        ]);
+
+        $note->update($data);
+
+        return to_route('note.show', $note)->with('message', 'Note was Updated');
     }
 
     /**
@@ -61,6 +74,8 @@ class NoteController extends Controller
      */
     public function destroy(Note $note)
     {
-        //
+        $note->delete();
+
+        return to_route('note.index')->with('message','Note was Deleted');
     }
 }
--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -7,9 +7,16 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 
 <body>
+    @session('message')
+        <div class="success-message">
+            {{ session('message') }}
+        </div>
+    @endsession
     {{ $slot }}
 </body>
 
